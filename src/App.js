94eced9c7d846962562f2550fbbfac18b4e0a4d1
DennyAndrian <andriandenny122@gmail.com>
2021-03-01 21:19:34 +0700

    1

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>assssssssssssssgwgwgwgwgw</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://api.whatsapp.com/send?text=https://sleepy-franklin-87c3f4.netlify.app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          share
-        </a>
+        <h2>Hallooo semuanya</h2>
       </header>
     </div>
   );
